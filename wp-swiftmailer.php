1cc301271eca5ab6683d6b8e5e7deaae2b831688
Ildar Bikmamatov <ildar@bayrell.org>
2021-04-19 07:49:13 +0000

    add site_transient_update_plugins

--- wp-swiftmailer.php
+++ wp-swiftmailer.php
@@ -1,22 +1,34 @@
 <?php
 /**
  * Plugin Name: Wordpress SwiftMailer Plugin
- * Plugin URI:  https://github.com/elberos/wp-swiftmailer
  * Description: SwiftMailer
  * Version:     5.4.12
  * License:     MIT
  */
  
- 
-if ( !function_exists( 'wp_swiftmailer_load' ) ) 
-{
-
-function wp_swiftmailer_load()
-{
-	if (!class_exists(Swift_Message::class))
-	{
-		require_once __DIR__ . "/vendor/autoload.php";
-	}
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
 }
 
+if ( !function_exists( 'wp_swiftmailer_load' ) ) 
+{
+	function wp_swiftmailer_load()
+	{
+		if (!class_exists(Swift_Message::class))
+		{
+			require_once __DIR__ . "/vendor/autoload.php";
+		}
+	}
+
+	/* Remove plugin updates */
+	add_filter( 'site_transient_update_plugins', 'wp_swiftmailer_filter_plugin_updates' );
+	function wp_swiftmailer_filter_plugin_updates($value)
+	{
+		$name = plugin_basename(__FILE__);
+		if (isset($value->response[$name]))
+		{
+			unset($value->response[$name]);
+		}
+		return $value;
+	}
 }
